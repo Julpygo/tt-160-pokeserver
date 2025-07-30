5c1e7e1fd98750ebc043f749fb3cd3bdb11327ea
julpygo <gomez.julian@correounivalle.edu.co>
2025-07-29 21:36:53 -0500

    creación controlador getPokemons y manejo de error nombres duplicados

--- controllers/pokemon.controllers.js
+++ controllers/pokemon.controllers.js
@@ -11,7 +11,23 @@ const createPokemon = async (req,res)=>{
         return res.status(201).json(pokemon)
     } catch (error) {
         console.error(error);
+        if(error.code == 11000){
+            return res.status(500).json({
+                message:"Ya existe el nombre del pokemon",
+                error:error
+                })
+        }
         return res.status(error.status).json(error)   
     }
 }
-export {hola,createPokemon}
+const getPokemons = async (req,res)=>{
+    try {
+        const pokemons = await pokemonModel.find()
+        return res.status(200).json(pokemons)
+    } catch (error) {
+        console.error(error);
+        return res.status(error.status).json(error) 
+    }
+}
+
+export {hola,createPokemon,getPokemons}
--- routes/pokemon.routes.js
+++ routes/pokemon.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import {hola, createPokemon} from "../controllers/pokemon.controllers.js"
+import {hola, createPokemon, getPokemons} from "../controllers/pokemon.controllers.js"
 const router = Router()
 
 router.get("/hola", hola)
 router.post("/create",createPokemon)
+router.get("/", getPokemons)
 
 export default router
